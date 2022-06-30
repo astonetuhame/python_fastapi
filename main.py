5c570e26b384ac0e1ca503b73bed64a64ab338f3
astonetuhame <astonetuhame@gmail.com>
2022-06-30 12:37:13 +0300

    Get all

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+movies = [{"title":"Batman", "year":2021}, {"title":"Joker", "year":2022} ]
+
+
+
 @app.get("/")
 async def root():
     return {"message":"welcome"}
+
+@app.get("/movies")
+def get_movies():
+    return movies
